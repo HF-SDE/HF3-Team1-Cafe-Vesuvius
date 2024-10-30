d3f1a079dc575534677ae5a8b641c1234fccd209
MN-House4it <magn9814@edu.sde.dk>
2024-10-30 13:18:42 +0100

    Add auth files and rate limit

---
+++ backend/src/api/services/auth.service.ts
@@ -0,0 +1,133 @@
+import jwt from 'jsonwebtoken';
+import { Request } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { accessToken } from '@controllers/auth.controller';
+
+const prisma = new PrismaClient();
+
+const generateToken = (user: object, ip: string | null, expiration: string, secret: string): string => {
+    return jwt.sign({ user }, secret + ip, { expiresIn: expiration });
+};
+
+// Change user type back to User
+export const generateUserTokens = async (user: any, req: Request) => {
+    const ip = req.socket.remoteAddress;
+    const newAccessToken = generateToken({ id: user.id, username: user.username }, ip, process.env.ACCESS_TOKEN_EXPIRATION || '5m', process.env.ACCESS_TOKEN_SECRET as string);
+    const newRefreshToken = generateToken({ id: user.id, username: user.username }, ip, process.env.REFRESH_TOKEN_EXPIRATION || '3d', process.env.REFRESH_TOKEN_SECRET as string);
+
+    const newRefreshTokenInDB = await prisma.refreshToken.create({
+        data: { token: newRefreshToken, userId: user.id },
+    });
+    await prisma.accessToken.create({
+        data: { token: newAccessToken, userId: user.id, refreshTokenId: newRefreshTokenInDB.id, sessionId: newRefreshTokenInDB.sessionId },
+    });
+
+    return {
+        accessToken: { token: newAccessToken, authType: 'bearer' },
+    };
+};
+
+
+// Reomove the tokens for the session in the DB
+export const invalidateRefreshToken = async (refreshToken: string) => {
+    const refreshTokenInDb = await prisma.refreshToken.findUnique({ where: { token: refreshToken } });
+    await prisma.accessToken.deleteMany({ where: { refreshTokenId: refreshTokenInDb?.id } });
+    await prisma.refreshToken.deleteMany({ where: { token: refreshToken } });
+};
+
+export const invalidateAllTokensForUser = async (userId: string) => {
+    await prisma.accessToken.deleteMany({ where: { userId: userId } });
+    await prisma.refreshToken.deleteMany({ where: { userId: userId } });
+};
+
+// Gets the newest refresh token from the DB with the same sessionId as the access token
+export const getRefreshToken = async (accessToken: string, req: Request) => {
+    const ip = req.socket.remoteAddress;
+
+    const user = jwt.verify(accessToken, (process.env.ACCESS_TOKEN_SECRET as string) + ip, { ignoreExpiration: true });
+    if (!user) return null;
+
+    const accesTokenInDb = await prisma.accessToken.findUnique({
+        where: { token: accessToken },
+    });
+
+    if (!accesTokenInDb) {
+        // The access token was not found in the DB
+        return null;
+    }
+
+    // Find the newest accessToken
+    const newestAccessToken = await prisma.accessToken.findFirst({
+        where: { sessionId: accesTokenInDb.sessionId },
+        orderBy: { createdAt: 'desc' },
+    });
+
+    // Check if the newest access token matches the provided access token
+    if (newestAccessToken && newestAccessToken.token === accessToken) {
+        // Gets the newest refresh token to return
+        const newestRefreshToken = await prisma.refreshToken.findFirst({
+            where: { sessionId: accesTokenInDb.sessionId },
+            orderBy: { createdAt: 'desc' },
+        });
+
+        if (newestRefreshToken) {
+            // Return the new refresh token
+            return {
+                refreshToken: { token: newestRefreshToken.token },
+            };
+        } else {
+            // There was no available refresh token for that session
+            return null;
+        }
+    } else {
+        // The access token was either not found in the DB or not the newest.
+        // All tokens for this user have been revoked
+        await invalidateAllTokensForUser(accesTokenInDb.userId);
+        return null;
+    }
+};
+
+
+export const refreshUserTokens = async (refreshToken: string, req: Request) => {
+    const ip = req.socket.remoteAddress;
+
+    const user = jwt.verify(refreshToken, (process.env.REFRESH_TOKEN_SECRET as string) + ip);
+    if (!user) return null;
+
+    const refreshTokenInDb = await prisma.refreshToken.findUnique({ where: { token: refreshToken } });
+    const newestRefreshToken = await prisma.refreshToken.findFirst({
+        where: { sessionId: refreshTokenInDb?.sessionId },
+        orderBy: { createdAt: 'desc' },
+    });
+
+    if (newestRefreshToken && newestRefreshToken.token === refreshToken) {
+        const newAccessToken = generateToken(user as object, ip, process.env.ACCESS_TOKEN_EXPIRATION || '5m', process.env.ACCESS_TOKEN_SECRET as string);
+        const newRefreshToken = generateToken(user as object, ip, process.env.REFRESH_TOKEN_EXPIRATION || '3d', process.env.REFRESH_TOKEN_SECRET as string);
+
+        const newRefreshEntry = await prisma.refreshToken.create({
+            data: { token: newRefreshToken, userId: (user as any).id, sessionId: refreshTokenInDb?.sessionId },
+        });
+        await prisma.accessToken.create({
+            data: { token: newAccessToken, userId: (user as any).id, refreshTokenId: newRefreshEntry.id, sessionId: newRefreshEntry.sessionId },
+        });
+
+        return {
+            accessToken: { token: newAccessToken, authType: 'bearer' },
+        };
+    }
+    
+    await prisma.accessToken.deleteMany({ where: { userId: refreshTokenInDb?.userId } });
+    await prisma.refreshToken.deleteMany({ where: { userId: refreshTokenInDb?.userId } });
+    return null;
+};
+
+// To check if a string is base64 encoded
+export const isBase64 = (str: string) => {
+    try {
+        return Buffer.from(str, 'base64').toString('base64') === str;
+    } catch (e) {
+        return false;
+    }
+  };
+
+export default { generateUserTokens, invalidateRefreshToken, getRefreshToken: getRefreshToken, refreshUserTokens, isBase64 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,26 @@ import config from '@config';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import { rateLimit } from 'express-rate-limit'
+
+import authRoutes from './api/routes/auth.routes';
+
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 60 minutes
+	limit: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
 
 const app = express();
 app.use(cors({credentials: true, origin: config.WHITELISTED_ORIGINS.split(' ')}));
 app.use(helmet());
 app.use(bodyParser.json({}));
 app.use(passport.initialize());
+app.use(limiter)
 
 //Insert all routes here
+//app.use('/', authRoutes);
 
 app.listen(config.PORT, () => {
     console.log(`Server is running on ${config.PORT}`);
