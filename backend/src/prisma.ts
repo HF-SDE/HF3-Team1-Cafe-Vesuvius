010fa2fcd76c54029559e727bcfba120ddf0580c
phiko-misc <phgu03@gmail.com>
2024-10-31 14:33:31 +0100

    🧹 chore(prisma): remove unnecessary ESLint disable comments

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
