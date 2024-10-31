07b8fe88fd81152288803a07818b35f5b950a380
phiko-misc <phgu03@gmail.com>
2024-10-31 08:35:13 +0100

    🐞 fix(*): Add all the stuff
    Docker should now work

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,13 @@
-import path from 'path';
 import * as dotenv from 'dotenv';
-dotenv.config({ path: path.join(__dirname, '../', ".env") });
+import path from 'path';
 
-const config = { 
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3001,
-    WHITELISTED_ORIGINS: process.env.WHITELISTED_ORIGINS || 'localhost:3000 localhost:3001',
-}
+dotenv.config({ path: path.join(__dirname, '../', '.env') });
+
+const config = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 3001,
+  WHITELISTED_ORIGINS:
+    process.env.WHITELISTED_ORIGINS || 'localhost:3000 localhost:3001',
+};
 
 export default config;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import config from '@config';
-import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
 import passport from 'passport';
+import { rateLimit } from 'express-rate-limit'
+
+import authRoutes from './api/routes/auth.routes';
+
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 60 minutes
+	limit: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
+
+import config from '@config';
 
 const app = express();
-app.use(cors({credentials: true, origin: config.WHITELISTED_ORIGINS.split(' ')}));
+app.use(
+  cors({ credentials: true, origin: config.WHITELISTED_ORIGINS.split(' ') }),
+);
 app.use(helmet());
 app.use(bodyParser.json({}));
 app.use(passport.initialize());
+app.use(limiter)
 
 //Insert all routes here
+//app.use('/', authRoutes);
 
 app.listen(config.PORT, () => {
-    console.log(`Server is running on ${config.PORT}`);
+  console.log(`Server is running on ${config.PORT}`);
 });
