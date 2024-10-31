8c283fbb168ed368df7da591e6b4f39fe9a6edd2
Nima Badiei <nimabadiei@gmail.com>
2024-10-31 08:33:01 +0100

    Formatted with prettier

--- backend/src/api/controllers/auth.controller.ts
+++ backend/src/api/controllers/auth.controller.ts
@@ -1,171 +1,181 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 //import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 import passport from 'passport';
+
 import { PrismaClient } from '@prisma/client';
+
 import authService from '../services/auth.service';
 
 const prisma = new PrismaClient();
 
 export const signUp = async (req: Request, res: Response) => {
-    res.json({ message: 'Signup successful' });
+  res.json({ message: 'Signup successful' });
 };
 
-export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const { username, password } = req.body;
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { username, password } = req.body;
 
-        if (!username?.trim() || !password?.trim()) {
-            res.status(400).json({
-                status: 'MissingCredentials',
-                message: 'Some credentials missing',
-            });
-            return;
-        }
-
-        if (!authService.isBase64(password)) {
-            res.status(401).json({
-                status: 'InvalidCredentials',
-                message: 'Username or Password was not correct',
-            });
-        }
-
-        const decodedPassword = Buffer.from(password, 'base64').toString();
-        req.body.password = decodedPassword;
-
-        // Change user back to type User
-        passport.authenticate('login', async (err: any, user: any | false) => {
-            if (err || !user) {
-                res.status(401).json({
-                    status: 'InvalidCredentials',
-                    message: 'Username or Password was not correct',
-                });
-                return;
-            }
-
-            req.login(user, { session: false }, async (error) => {
-                if (error) {
-                    next(error);
-                    return;
-                }
-
-                try {
-                    const tokens = await authService.generateUserTokens(user, req);
-                    res.json(tokens);
-                } catch (tokenError) {
-                    res.status(500).json({
-                        status: 'TokenError',
-                        message: 'Failed to generate tokens',
-                    });
-                }
-            });
-        })(req, res, next);
-    } catch (error) {
-        res.status(500).json({
-            status: 'ServerError',
-            message: 'Something went wrong on our end',
-        });
+    if (!username?.trim() || !password?.trim()) {
+      res.status(400).json({
+        status: 'MissingCredentials',
+        message: 'Some credentials missing',
+      });
+      return;
     }
+
+    if (!authService.isBase64(password)) {
+      res.status(401).json({
+        status: 'InvalidCredentials',
+        message: 'Username or Password was not correct',
+      });
+    }
+
+    const decodedPassword = Buffer.from(password, 'base64').toString();
+    req.body.password = decodedPassword;
+
+    // Change user back to type User
+    passport.authenticate('login', async (err: any, user: any | false) => {
+      if (err || !user) {
+        res.status(401).json({
+          status: 'InvalidCredentials',
+          message: 'Username or Password was not correct',
+        });
+        return;
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) {
+          next(error);
+          return;
+        }
+
+        try {
+          const tokens = await authService.generateUserTokens(user, req);
+          res.json(tokens);
+        } catch (tokenError) {
+          res.status(500).json({
+            status: 'TokenError',
+            message: 'Failed to generate tokens',
+          });
+        }
+      });
+    })(req, res, next);
+  } catch (error) {
+    res.status(500).json({
+      status: 'ServerError',
+      message: 'Something went wrong on our end',
+    });
+  }
 };
 
 export const logout = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { token: refreshToken } = req.body;
+  try {
+    const { token: refreshToken } = req.body;
 
-        if (!refreshToken) {
-            res.status(400).json({
-                status: 'MissingData',
-                message: 'Missing refresh token',
-            });
-            return;
-        }
-        
-        await authService.invalidateRefreshToken(refreshToken);
-        res.status(200).json({ message: 'Logout successful' });
-    } catch (error) {
-        res.status(500).json({
-            status: 'ServerError',
-            message: 'Something went wrong on our end',
-        });
+    if (!refreshToken) {
+      res.status(400).json({
+        status: 'MissingData',
+        message: 'Missing refresh token',
+      });
+      return;
     }
+
+    await authService.invalidateRefreshToken(refreshToken);
+    res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    res.status(500).json({
+      status: 'ServerError',
+      message: 'Something went wrong on our end',
+    });
+  }
 };
 
-export const accessToken = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { token: refreshToken } = req.body;
+export const accessToken = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { token: refreshToken } = req.body;
 
-        if (!refreshToken) {
-            res.status(400).json({
-                status: 'MissingData',
-                message: 'Missing authentication',
-            });
-            return;
-        }
-
-        const newTokens = await authService.refreshUserTokens(refreshToken, req);
-        if (!newTokens) {
-            res.status(403).json({ 
-                status: 'Unauthorized',
-                message: 'Token expired or invalid',
-            });
-            return;
-        }
-
-        res.json(newTokens);
-    } catch (error) {
-        res.status(500).json({
-            status: 'ServerError',
-            message: 'Something went wrong on our end',
-        });
+    if (!refreshToken) {
+      res.status(400).json({
+        status: 'MissingData',
+        message: 'Missing authentication',
+      });
+      return;
     }
+
+    const newTokens = await authService.refreshUserTokens(refreshToken, req);
+    if (!newTokens) {
+      res.status(403).json({
+        status: 'Unauthorized',
+        message: 'Token expired or invalid',
+      });
+      return;
+    }
+
+    res.json(newTokens);
+  } catch (error) {
+    res.status(500).json({
+      status: 'ServerError',
+      message: 'Something went wrong on our end',
+    });
+  }
 };
 
+export const refreshToken = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
 
-export const refreshToken = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            res.status(400).json({
-                status: 'MissingData',
-                message: 'Missing authentication header',
-            });
-            return;
-        }
-        
-        const tokenParts = authHeader.split(' ');
-        if (tokenParts.length !== 2 || tokenParts[0].toLowerCase() !== 'bearer') {
-            res.status(400).json({
-                status: 'InvalidData',
-                message: 'Invalid authentication format. Expected Bearer token.',
-            });
-            return;
-        }
-        
-        const accessToken = tokenParts[1]; // Get the actual access token
-        
-
-        if (!accessToken) {
-            res.status(400).json({
-                status: 'MissingData',
-                message: 'Missing authentication',
-            });
-            return;
-        }
-
-        const refreshToken = await authService.getRefreshToken(accessToken, req);
-        if (!refreshToken) {
-            res.status(403).json({ 
-                status: 'Unauthorized',
-                message: 'Token expired or invalid',
-            });
-            return;
-        }
-
-        res.json({token: refreshToken});
-    } catch (error) {
-        res.status(500).json({
-            status: 'ServerError',
-            message: 'Something went wrong on our end',
-        });
+    if (!authHeader) {
+      res.status(400).json({
+        status: 'MissingData',
+        message: 'Missing authentication header',
+      });
+      return;
     }
+
+    const tokenParts = authHeader.split(' ');
+    if (tokenParts.length !== 2 || tokenParts[0].toLowerCase() !== 'bearer') {
+      res.status(400).json({
+        status: 'InvalidData',
+        message: 'Invalid authentication format. Expected Bearer token.',
+      });
+      return;
+    }
+
+    const accessToken = tokenParts[1]; // Get the actual access token
+
+    if (!accessToken) {
+      res.status(400).json({
+        status: 'MissingData',
+        message: 'Missing authentication',
+      });
+      return;
+    }
+
+    const refreshToken = await authService.getRefreshToken(accessToken, req);
+    if (!refreshToken) {
+      res.status(403).json({
+        status: 'Unauthorized',
+        message: 'Token expired or invalid',
+      });
+      return;
+    }
+
+    res.json({ token: refreshToken });
+  } catch (error) {
+    res.status(500).json({
+      status: 'ServerError',
+      message: 'Something went wrong on our end',
+    });
+  }
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,19 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
 import helmet from 'helmet';
 import passport from 'passport';
-import { rateLimit } from 'express-rate-limit'
-
-import authRoutes from './api/routes/auth.routes';
-
-const limiter = rateLimit({
-	windowMs: 60 * 60 * 1000, // 60 minutes
-	limit: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-})
 
 import config from '@config';
+import authRoutes from '@routes/auth.routes';
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  limit: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
 
 const app = express();
 app.use(
@@ -23,7 +22,7 @@ app.use(
 app.use(helmet());
 app.use(bodyParser.json({}));
 app.use(passport.initialize());
-app.use(limiter)
+app.use(limiter);
 
 //Insert all routes here
 //app.use('/', authRoutes);
