d6d7025ac27a0993ebdd8c61dcb3cff5a67b9f67
MN-House4it <magn9814@edu.sde.dk>
2024-10-31 11:09:31 +0100

    Add referance to passport file in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,8 @@ import passport from 'passport';
 import config from '@config';
 import authRoutes from '@routes/auth.routes';
 
+import './passport';
+
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
   limit: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
