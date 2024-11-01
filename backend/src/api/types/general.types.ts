fff46ced76ca404b79e0acc0188cbc7a0c548059
Nima Badiei <nimabadiei@gmail.com>
2024-11-01 08:45:45 +0100

    Formatted with prettier

--- backend/src/api/types/general.types.ts
+++ backend/src/api/types/general.types.ts
@@ -1,29 +1,29 @@
-export interface IAPIResponse { 
-    status: Status;
-    message?: string;
+export interface IAPIResponse {
+  status: Status;
+  message?: string;
 }
 
 export enum Status {
-    UnAuthorized = 'Unauthorized',
-    Forbidden = 'Forbidden',
-    Success = 'Success',
-    Failed = 'Failed',
-    Found = 'Found',
-    NotFound = 'NotFound',
-    Created = 'Created',
-    CreationFailed = 'CreationFailed',
-    Deleted = 'Deleted',
-    DeleteFailed = 'DeletionFailed',
-    Updated = 'Updated',
-    UpdateFailed = 'UpdateFailed',
-    MissingDetails = 'MissingDetails',
-    InvalidDetails = 'InvalidDetails',
-    MissingCredentials = 'MissingCredentials',
-    InvalidCredentials = 'InvalidCredentials',
+  Unauthorized = 'Unauthorized',
+  Forbidden = 'Forbidden',
+  Success = 'Success',
+  Failed = 'Failed',
+  Found = 'Found',
+  NotFound = 'NotFound',
+  Created = 'Created',
+  CreationFailed = 'CreationFailed',
+  Deleted = 'Deleted',
+  DeleteFailed = 'DeletionFailed',
+  Updated = 'Updated',
+  UpdateFailed = 'UpdateFailed',
+  MissingDetails = 'MissingDetails',
+  InvalidDetails = 'InvalidDetails',
+  MissingCredentials = 'MissingCredentials',
+  InvalidCredentials = 'InvalidCredentials',
 }
 
 export interface APIResponse<T> {
-    status: Status;
-    message?: string;
-    data: T | null;
+  status: Status;
+  message?: string;
+  data: T | null;
 }
