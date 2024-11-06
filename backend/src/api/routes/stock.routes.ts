d2b408124e41a55ca09b34988c7aee0f7c75bcf0
phiko-misc <phgu03@gmail.com>
2024-11-06 12:09:08 +0100

    ✨ feat: Update stock routes to use alias for passport and enhance TypeScript configuration

--- backend/src/api/routes/stock.routes.ts
+++ backend/src/api/routes/stock.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
 import * as StockController from '@controllers/stock.controller';
-import { authenticateJwt } from 'passport';
+import { authenticateJwt } from '@passport';
 
 const router = Router();
 
-router.get('/', authenticateJwt , StockController.getStock);
+router.get('/', authenticateJwt, StockController.getStock);
 
 export default router;
