eddce4ddce15b7129f42feea60bcbe195592f965
Nima Badiei <nimabadiei@gmail.com>
2024-11-06 14:51:06 +0100

    Added JSDocs to a function

--- backend/src/api/controllers/table.controller.ts
+++ backend/src/api/controllers/table.controller.ts
@@ -1,14 +1,28 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 
 import prisma from '@prisma-instance';
 
-export async function getTables(req: Request, res: Response) {
+/**
+ * Controller to get all tables
+ * @async
+ * @param {Request} req - The request object
+ * @param {Response} res - The response object
+ * @returns {Promise<void>} The response object
+ */
+export async function getTables(req: Request, res: Response): Promise<void> {
   const tables = await prisma.table.findMany();
 
   res.json(tables);
 }
 
-export async function getTable(req: Request, res: Response) {
+/**
+ * Controller to get a table by id
+ * @async
+ * @param {Request} req - The request object
+ * @param {Response} res - The response object
+ * @returns {Promise<void>} The response object
+ */
+export async function getTable(req: Request, res: Response): Promise<void> {
   const { id } = req.params;
 
   const table = await prisma.table.findUnique({ where: { id } });
