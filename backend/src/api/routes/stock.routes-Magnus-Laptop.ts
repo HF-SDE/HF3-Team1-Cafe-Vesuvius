472bc28d4e796f1862c29206163e66dc304291ae
Nima Badiei <nimabadiei@gmail.com>
2024-11-13 08:43:57 +0100

    Deleted unused files

--- backend/src/api/routes/stock.routes-Magnus-Laptop.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import * as StockController from '@controllers/stock.controller';
-
-const router = Router();
-
-router.get('/', StockController.getStock);
-
-export default router;
