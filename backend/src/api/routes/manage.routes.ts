1f954e70eb285b72c252b3bf1be61f884117f859
phiko-misc <phgu03@gmail.com>
2024-11-13 14:07:35 +0100

    ✨ feat: Add GET endpoint for retrieving users in manage routes

--- backend/src/api/routes/manage.routes.ts
+++ backend/src/api/routes/manage.routes.ts
@@ -5,5 +5,6 @@ import * as ManageController from '@controllers/manage.controller';
 const router = Router();
 
 router.put('/password', ManageController.changePassword);
+router.get('/manage/user', ManageController.getUsers);
 
 export default router;
