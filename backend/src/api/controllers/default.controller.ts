1d7ce62bcb50fbd1c3658b3683d760e4d8add06b
Nima Badiei <nimabadiei@gmail.com>
2024-11-14 13:50:48 +0100

    Updated and improved the default controller & service

--- backend/src/api/controllers/default.controller.ts
+++ backend/src/api/controllers/default.controller.ts
@@ -1,70 +1,81 @@
 import { Request, Response } from 'express';
 
+import { ExpressFunction } from '@api-types/general.types';
 import { Prisma } from '@prisma/client';
 import * as DefaultService from '@services/default.service';
 import { getHttpStatusCode } from '@utils/Utils';
 
 type prismaModels = Uncapitalize<Prisma.ModelName>;
 
+// eslint-disable-next-line jsdoc/require-jsdoc
+function getModel(req: Request): prismaModels {
+  return req.baseUrl.replace('/', '') as prismaModels;
+}
+
 /**
  * Controller to get all
- * @async
- * @param {Request} req - The request object
- * @param {Response} res - The response object
- * @returns {Promise<void>} The response object
+ * @param {prismaModels} model - The Prisma model to get the records from.
+ * @returns {ExpressFunction} The response object
  */
-export async function getAll(req: Request, res: Response): Promise<void> {
-  const model = req.baseUrl.replace('/api/', '') as prismaModels;
-  const id = (req.query.id || req.params.id) as string | undefined;
+export function getAll(model?: prismaModels): ExpressFunction {
+  return async (req, res): Promise<void> => {
+    const { id } = req.params;
 
-  const response = await DefaultService.getAll(model, id);
+    const response = await DefaultService.getAll(model ?? getModel(req), id);
 
-  res.status(getHttpStatusCode(response.status)).json(response).end();
+    res.status(getHttpStatusCode(response.status)).json(response).end();
+  };
 }
 
 /**
  * Controller to create a record
- * @async
- * @param {Request} req - The request object
- * @param {Response} res - The response object
- * @returns {Promise<void>} The response object
+ * @param {prismaModels} model - The Prisma model to create the record with.
+ * @returns {ExpressFunction} The response object
  */
-export async function create(req: Request, res: Response): Promise<void> {
-  const model = req.baseUrl.replace('/api/', '') as prismaModels;
+export function create(model?: prismaModels): ExpressFunction {
+  return async (req: Request, res: Response): Promise<void> => {
+    const response = await DefaultService.create(
+      model ?? getModel(req),
+      req.body,
+    );
 
-  const response = await DefaultService.create(model, req.body);
-
-  res.status(getHttpStatusCode(response.status)).json(response).end();
+    res.status(getHttpStatusCode(response.status)).json(response).end();
+  };
 }
 
 /**
  * Controller to update a record
- * @async
- * @param {Request} req - The request object
- * @param {Response} res - The response object
- * @returns {Promise<void>} The response object
+ * @param {prismaModels} model - The Prisma model to update the record in.
+ * @returns {ExpressFunction} The response object
  */
-export async function update(req: Request, res: Response): Promise<void> {
-  const model = req.baseUrl.replace('/api/', '') as prismaModels;
-  const { id } = req.params;
+export function update(model?: prismaModels): ExpressFunction {
+  return async (req, res) => {
+    const { id } = req.params;
 
-  const response = await DefaultService.update(model, id, req.body);
+    const response = await DefaultService.update(
+      model ?? getModel(req),
+      id,
+      req.body,
+    );
 
-  res.status(getHttpStatusCode(response.status)).json(response).end();
+    res.status(getHttpStatusCode(response.status)).json(response).end();
+  };
 }
 
 /**
  * Controller to delete a record
- * @async
- * @param {Request} req - The request object
- * @param {Response} res - The response object
- * @returns {Promise<void>} The response object
+ * @param {prismaModels} model - The Prisma model to delete the record from.
+ * @returns {ExpressFunction} The response object
  */
-export async function deleteRecord(req: Request, res: Response): Promise<void> {
-  const model = req.baseUrl.replace('/api/', '') as prismaModels;
-  const { id } = req.params;
+export function deleteRecord(model?: prismaModels): ExpressFunction {
+  return async (req, res) => {
+    const { id } = req.params;
 
-  const response = await DefaultService.deleteRecord(model, id);
+    const response = await DefaultService.deleteRecord(
+      model ?? getModel(req),
+      id,
+    );
 
-  res.status(getHttpStatusCode(response.status)).json(response).end();
+    res.status(getHttpStatusCode(response.status)).json(response).end();
+  };
 }
