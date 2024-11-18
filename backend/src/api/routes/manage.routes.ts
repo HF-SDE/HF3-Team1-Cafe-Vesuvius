831cf6279822585ae57e4f2208aa9189c59bd42d
Nima Badiei <nimabadiei@gmail.com>
2024-11-18 13:06:26 +0100

    refactor(routes): consolidate manage routes and remove permission routes

--- backend/src/api/routes/manage.routes.ts
+++ backend/src/api/routes/manage.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 
+import { getAll } from '@controllers/default.controller';
 import * as ManageController from '@controllers/manage.controller';
 import { verifyJWT } from '@middlewares/authenticate.mw';
 import { isAllowed } from '@middlewares/isAllowed.mw';
+import { validateParams } from '@middlewares/validate.mw';
 
 const router = Router();
 
-router.get('/user', verifyJWT, isAllowed(['administrator:users:view']), ManageController.getUsers);
+router.use('/', verifyJWT);
+router.use('/:id', validateParams);
+
+router.get(
+  '/user',
+  isAllowed(['administrator:users:view']),
+  ManageController.getUsers,
+);
+router.get(['/', '/:id'], isAllowed(['permission:view']), getAll());
 
 export default router;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,6 @@ import config from '@config';
 import authRoutes from '@routes/auth.routes';
 import manageRoutes from '@routes/manage.routes';
 import orderRoutes from '@routes/order.routes';
-import permissionRoutes from '@routes/permission.routes';
 import profileRoutes from '@routes/profile.routes';
 import reservationRoutes from '@routes/reservation.routes';
 import resetRoutes from '@routes/reset.routes';
@@ -48,7 +47,6 @@ app.use(`/reservation`, reservationRoutes);
 app.use(`/order`, orderRoutes);
 app.use(`/manage`, manageRoutes);
 app.use(`/profile`, profileRoutes);
-app.use(`/permission`, permissionRoutes);
 
 app.get('/ping', (req, res) => {
   res.json({
