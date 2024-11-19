4d54f4973322c111f5885ebada39879420f5ca18
MN-House4it <magn9814@edu.sde.dk>
2024-11-19 13:43:21 +0100

    Upload models

---
+++ app/models/userModels.ts
@@ -0,0 +1,13 @@
+export interface UserProfile {
+  id: string;
+  username: string;
+  email: string;
+  initials: string;
+  name: string;
+  permissions: Permission[];
+}
+
+export interface Permission {
+  code: string;
+  description: string;
+}
