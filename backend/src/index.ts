05a93ec0955c5583510c97ec184ad72ebe25fb39
phiko-misc <phgu03@gmail.com>
2024-11-19 09:06:00 +0100

    🐞 fix: update CORS configuration and add PostgreSQL database URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,7 +30,7 @@ const app = express();
 app.set('trust proxy', 1);
 app.set('json spaces', 4);
 
-app.use(cors({ origin: true }));
+app.use(cors());
 
 app.use(helmet());
 app.use(bodyParser.json({}));
