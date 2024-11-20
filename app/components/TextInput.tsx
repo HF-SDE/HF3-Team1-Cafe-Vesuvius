7f52542917da428d8c0785e98903de49eacdb169
phiko-misc <phgu03@gmail.com>
2024-11-20 09:41:44 +0100

    ✨ feat: Update TextInput component with additional props and documentation

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -1,25 +1,51 @@
-import React from "react";
-import { TextInput } from "react-native-paper";
+import React, { HTMLInputAutoCompleteAttribute } from "react";
+import { TextInput, TextInputProps } from "react-native-paper";
 import { useThemeColor } from "@/hooks/useThemeColor";
+import { TextInputLabelProp } from "react-native-paper/lib/typescript/components/TextInput/types";
+import { EnterKeyHintTypeOptions, InputModeOptions, StyleProp } from "react-native";
+import { TextStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 
 interface InputProps {
-  label: string;
+  label: TextInputLabelProp;
   value: string;
   onChange: (value: string) => void;
-  style?: object;
+  style?: StyleProp<TextStyle>;
+  clearButtonMode: TextInputProps["clearButtonMode"];
+  autoComplete: TextInputProps["autoComplete"];
+  autoCapitalize: TextInputProps["autoCapitalize"];
+  clearTextOnFocus: TextInputProps["clearTextOnFocus"];
+  enablesReturnKeyAutomatically: TextInputProps["enablesReturnKeyAutomatically"];
+  enterKeyHint: EnterKeyHintTypeOptions;
+  inputMode: InputModeOptions;
 }
 
-const CustomTextInput: React.FC<InputProps> = ({
-  label,
-  value,
-  onChange,
-  style,
-}) => {
+
+/**
+ * Custom text input component
+ *
+ * @example
+ * <CustomTextInput label="Name" value={name} onChange={setName} />
+ * 
+ * @param {TextInputLabelProp} label - label for the text input @see https://reactnative.dev/docs/textinput#label
+ * @param {string} value - value for the text input @see https://reactnative.dev/docs/textinput#value
+ * @param {(value: string) => void} onChange - function to handle text input change event @see https://reactnative.dev/docs/textinput#onchangetext
+ * @param {StyleProp<TextStyle>} style - custom style for the text input @see https://reactnative.dev/docs/textinput#style
+ * @param {TextInputProps["clearButtonMode"]} [clearButtonMode="never"] [IOS ONLY] - default is "never". @see https://reactnative.dev/docs/textinput#clearbuttonmode-ios
+ * @param {TextInputProps["autoComplete"]} [autoComplete="off"] - default is "off". @see https://reactnative.dev/docs/textinput#autocomplete
+ * @param {TextInputProps["autoCapitalize"]} [autoCapitalize="sentences"] - default is "sentences". @see https://reactnative.dev/docs/textinput#autocapitalize
+ * @param {TextInputProps["clearTextOnFocus"]} [clearTextOnFocus=true] [IOS ONLY] - default is true. @see https://reactnative.dev/docs/textinput#cleartextonfocus-ios
+ * @param {TextInputProps["enablesReturnKeyAutomatically"]} [enablesReturnKeyAutomatically=false] [IOS ONLY] - default is false.  @see https://reactnative.dev/docs/textinput#enablesreturnkeyautomatically-ios
+ * @param {EnterKeyHintTypeOptions} [enterKeyHint="next"] - default is "next". What enter keys should be used for. @see https://reactnative.dev/docs/textinput#enterkeyhint
+ * @param {InputModeOptions} [inputMode="text"] - default is "text". Helps to determine what keyboardType should be used. @see https://reactnative.dev/docs/textinput#inputmode
+ * @returns {*}
+ */
+export default function CustomTextInput({ label, value, onChange, style, clearButtonMode = "never", autoComplete = "off", autoCapitalize = "sentences", clearTextOnFocus = true, enablesReturnKeyAutomatically = false, enterKeyHint = "next", inputMode = "text"  }: InputProps) {
   const BackgroundColor = useThemeColor({}, "background");
   const TextColor = useThemeColor({}, "text");
   const PrimaryColor = useThemeColor({}, "primary");
   const SecondaryColor = useThemeColor({}, "secondary");
   const AccentColor = useThemeColor({}, "accent");
+
 
   return (
     <TextInput
@@ -30,8 +56,14 @@ const CustomTextInput: React.FC<InputProps> = ({
       activeOutlineColor={PrimaryColor}
       outlineColor={SecondaryColor}
       onChangeText={onChange}
+      clearButtonMode={clearButtonMode}
+      autoComplete={autoComplete}
+      autoCapitalize={autoCapitalize}
+      clearTextOnFocus={clearTextOnFocus}
+      enablesReturnKeyAutomatically={enablesReturnKeyAutomatically}
+      enterKeyHint={enterKeyHint}
+      inputMode={inputMode}
     />
   );
 };
 
-export default CustomTextInput;
