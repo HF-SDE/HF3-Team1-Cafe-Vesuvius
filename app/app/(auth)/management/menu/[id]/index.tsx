fddf153aa18fd8166bd9d958a4a393eeb34ce644
MN-House4it <magn9814@edu.sde.dk>
2024-12-13 08:53:29 +0100

    Add alert to the delete menu button

--- app/app/(auth)/management/menu/[id]/index.tsx
+++ app/app/(auth)/management/menu/[id]/index.tsx
@@ -12,6 +12,7 @@ import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { router } from "expo-router";
 import LoadingPage from "@/components/LoadingPage";
 import CheckPermission from "@/components/CheckPermission";
+import Alert from "@/components/Alert";
 
 import { MenuModel } from "@/models/MenuModel";
 
@@ -71,9 +72,26 @@ export default function EditCreateUserPage() {
               color={theme.secondary}
               onPress={async () => {
                 if (id && id !== "new") {
-                  await triggerHapticFeedback();
-                  await deleteMenu(id);
-                  navigation.goBack();
+                  Alert(
+                    "Confirm Deletion",
+                    `Are you sure you want to delete ${menuItem.name}?`,
+                    [
+                      {
+                        text: "Cancel",
+                        style: "cancel",
+                        onPress: () => {},
+                      },
+                      {
+                        text: "Delete",
+                        style: "destructive",
+                        onPress: async () => {
+                          await triggerHapticFeedback();
+                          await deleteMenu(id);
+                          navigation.goBack();
+                        },
+                      },
+                    ]
+                  );
                 }
               }}
               style={{ marginRight: 16 }}
